bd3b400a4f37ecbe4a65cd3df8b1baf2022f9d30
Navi sureka <navisureka23@gmail.com>
2022-09-30 02:17:20 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV!== "production"){
-    require('dotenv').config({ path: './config/dev.env' })
-}
+// if(process.env.NODE_ENV!== "production"){
+//     require('dotenv').config({ path: './config/dev.env' })
+// }
 
 
 const express= require("express");
