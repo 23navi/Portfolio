1c5795bf6754ccfd2fd65bdacfd8ba096caaf4eb
Navi sureka <navisureka23@gmail.com>
2022-10-20 00:33:07 +0530

    added some project add edit features

--- src/allProjects/models/projectModel.js
+++ src/allProjects/models/projectModel.js
@@ -2,20 +2,23 @@ const mongoose= require("mongoose");
 
 const projectSchema = new mongoose.Schema({
     
-    likes:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}],
+    likes:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}], //Will implement later
+
     name:{type:String},
     title:{type:String},
     images:[{url:String}],
     descriptionBasic:{type:String},
     descriptionMain:{type:String},
     reviews: [{
-
         type:mongoose.Schema.Types.ObjectId,  // reviews will be an array of obj that refer to reviews
         ref:"Review"                   // We ref to the name of the model as we defined.
-  
     }],
     projectStart:String,
     projectEnd:String,
+    ongoing:Boolean,
+    hasLiveLink:Boolean,
+    liveLink:String,
+    hasGithub:Boolean,
     tags:[String]
 
 })
